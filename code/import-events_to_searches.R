117885e3a0337a7e9a0878797470248de18c4d19
guilherme@copin.ufcg.edu.br <guilhermemgadelha@gmail.com>
2018-04-26 17:44:19 -0300

    first question answered

--- code/import-events_to_searches.R
+++ code/import-events_to_searches.R
@@ -4,23 +4,23 @@ library(here)
 
 message("Lendo dados brutos de eventos")
 
-events = read_csv("https://github.com/wikimedia-research/Discovery-Hiring-Analyst-2016/raw/master/events_log.csv.gz")
+events = read_csv(here::here("data/events_log.csv.gz"))
 
-# events = events %>% slice(1:5e4) # Útil para testar código em dados pequenos. Comente na hora de processá-los para valer.
+events = events %>% slice(1:1e6) # Útil para testar código em dados pequenos. Comente na hora de processá-los para valer.
 
 message("Transformando em dados por busca")
 
 events = events %>% 
     group_by(session_id) %>% 
     arrange(timestamp) %>% 
-    mutate(search_index = cumsum(action == "searchResultPage")) # contador para as buscas nessa sessão.
+    mutate(num_searches = cumsum(action == "searchResultPage")) # contador para as buscas nessa sessão.
 
 searches = events %>% 
-    group_by(session_id, search_index) %>% 
+    group_by(session_id, num_searches) %>% 
     arrange(timestamp) %>% 
     summarise(
-        session_start_timestamp = first(timestamp),
-        session_start_date = ymd_hms(first(timestamp)),
+        first_event_timestamp = first(timestamp),
+        first_event_date_time = ymd_hms(first(timestamp)),
         group = first(group), # eventos de uma mesma sessão são de um mesmo grupo
         results = max(n_results, na.rm = TRUE), # se não houver busca, retorna -Inf
         num_clicks = sum(action == "visitPage"), 
@@ -29,7 +29,7 @@ searches = events %>%
                              first(na.omit(result_position))
         )
     ) %>% 
-    filter(search_index > 0) # Apenas search sessions
+    filter(num_searches > 0) # Apenas search sessions
 
 out_file = here("data/search_data.csv")
 
